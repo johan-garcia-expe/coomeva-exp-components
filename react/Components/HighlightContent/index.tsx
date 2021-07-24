c5d2f5abba44d1426e440f65bb1faaddb4d8731a
Jose Gomez <jose.fgm1995@gmail.com>
2021-07-23 20:45:32 -0500

    feat(components): Card-Layout & Highlight-Content

---
+++ react/Components/HighlightContent/index.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { SliderLayout } from 'vtex.slider-layout';
+import { useCssHandles } from 'vtex.css-handles';
+import CardLayout, { CardLayoutProps } from '../CardLayout';
+
+type HighlightContentProps = {
+  items: CardLayoutProps[];
+  sliderMode: boolean;
+  sliderProps: any;
+};
+
+const CSS_HANDLES = ['highlightContent'];
+
+const HighlightContent = ({
+  items,
+  sliderMode,
+  sliderProps,
+}: HighlightContentProps) => {
+  const handles = useCssHandles(CSS_HANDLES);
+
+  return items ? (
+    <section className={handles.highlightContent}>
+      {sliderMode ? (
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        <SliderLayout {...sliderProps}>
+          {items.map((item, index) => (
+            <CardLayout
+              image={item.image}
+              secundaryImage={item.secundaryImage}
+              text={item.text}
+              link={item.link}
+              // eslint-disable-next-line react/no-array-index-key
+              key={`highlightContent-${index}`}
+            />
+          ))}
+        </SliderLayout>
+      ) : (
+        items.map((item, index) => (
+          <CardLayout
+            image={item.image}
+            secundaryImage={item.secundaryImage}
+            text={item.text}
+            link={item.link}
+            // eslint-disable-next-line react/no-array-index-key
+            key={`highlightContent-${index}`}
+          />
+        ))
+      )}
+    </section>
+  ) : null;
+};
+
+HighlightContent.schema = {
+  title: 'Slider de contenido destacado',
+  type: 'object',
+  properties: {
+    items: {
+      title: 'Elementos destacados',
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          image: {
+            title: 'Imagen principal',
+            type: 'string',
+            widget: {
+              'ui:widget': 'image-uploader',
+            },
+          },
+          secundaryImage: {
+            title: 'Imagen segundaria',
+            type: 'string',
+            widget: {
+              'ui:widget': 'image-uploader',
+            },
+          },
+          text: {
+            title: 'Texto para la card',
+            type: 'string',
+            widget: {
+              'ui:widget': 'image-uploader',
+            },
+          },
+          link: {
+            title: 'Link',
+            type: 'string',
+            widget: {
+              'ui:widget': 'image-uploader',
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export default HighlightContent;
